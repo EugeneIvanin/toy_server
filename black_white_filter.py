153526fab127b1de9b996d44b01743ad26b34d96
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2019-07-10 23:46:11 +0200

    Create black_white_filter.py

---
+++ black_white_filter.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import argparse
+parser = argparse.ArgumentParser(description='Great Description To Be Here')
+parser.add_argument('-location', action='store', help='Location of original image')
+args = parser.parse_args()
+im = Image.open(args.location)
+filtered_im = im.convert('L')
+filtered_im.save('result.jpg')
