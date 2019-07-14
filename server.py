b1b9da5f1c42829cec976c41f15f1ee8eef287ae
Eugene <34417631+EugeneIvanin@users.noreply.github.com>
2019-07-14 20:40:16 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,58 @@
+from PIL import Image
+import time
+import argparse
+import subprocess
+import sys
+import os
+
+
+
+web.config.debug = False
+web.config.session_parameters['cookie_path'] = '/'
+        
+
+urls = ("/", "index", 
+	"/result", "result")
+
+app = web.application(urls, globals())
+render = web.template.render('templates/', cache = False)
+
+
+if web.config.get('_session') is None:
+    session = web.session.Session(app, web.session.DiskStore('sessions'), {'filter_url': "", 'origin_url': ""})
+    web.config._session = session
+else:
+    session = web.config._session
+
+
+  
+class index:
+    def GET(self):
+        return render.index()
+
+
+    def POST(self):
+        x = web.input(myfile={})
+	
+	filedir = '/home/ubuntu/toy_server'
+        filepath = x.myfile.filename.replace('\\', '/')
+        fout = open(filedir + '/' + 'content.png', 'w')
+        fout.write(x.myfile.file.read())
+        fout.close()
+	im = Image.open('/home/ubuntu/toy_server/content.png')
+	filtered_im = im.convert('L')
+	time.sleep(8)
+	filtered_im.save('/home/ubuntu/toy_server/result.png')
+	
+        raise web.seeother('/result')
+  
+      
+class result:
+    def GET(self):
+        imageBinary = open("/home/ubuntu/toy_server/result.png",'rb').read()
+        return imageBinary
+
+
+
+if __name__ == "__main__":
+    app.run()
